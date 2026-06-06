6250334d547f3d3381d7525025acceb9386721a5
jf0443060-cloud <jf0443060@gmail.com>
2026-06-06 15:55:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 import Card from "./card.js";
 export default function App() {
+  let products = ["Laptop", "Mobile", "Keyboard"];
+  let products2 = [
+    {
+      productName: "computer",
+      price: 25000,
+      description: "this is a best product",
+    },
+    {
+      productName: "TabelFan",
+      price: 5000,
+      description: "this is a best product",
+    },
+  ];
   return (
     <div className="App">
-      <Card productName="pp1" price="10000" />
-      <Card productName="pp2" price="15000" />
+      {products2.map((p, i) => {
+        return (
+          <Card
+            productName={p.productName}
+            price={p.price}
+            des={p.description}
+          />
+        );
+      })}
     </div>
   );
 }
