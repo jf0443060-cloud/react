70f7685170d2f0f912380fb11b106514fc9e4f90
jf0443060-cloud <jf0443060@gmail.com>
2026-06-06 15:55:51 +0530

    Update Card.js

--- Card.js
+++ Card.js
@@ -1,12 +1,11 @@
 import "./styles.css";
 
 function Card(props) {
-  console.log(props);
   return (
-    <div>
-      <h1>{props.productName} </h1>
+    <div className="card">
+      <h1> {props.productName}</h1>
       <p> This ia a great smart phone</p>
-      <div> price: ${props.price} </div>
+      <div> price: $ {props.price} </div>
       <div> Add to Card</div>
     </div>
   );
