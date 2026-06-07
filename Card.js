2abf91cb6af0704ad6cf8533a05a2e22fce44ee0
jf0443060-cloud <jf0443060@gmail.com>
2026-06-07 16:11:35 +0530

    Update Card.js

--- Card.js
+++ Card.js
@@ -1,14 +1,34 @@
 import "./styles.css";
-
-function Card(props) {
+export default function List(props) {
   return (
-    <div className="card">
-      <h1> {props.productName}</h1>
-      <p> This ia a great smart phone</p>
-      <div> price: $ {props.price} </div>
-      <div> Add to Card</div>
+    <div class="listContanier">
+      <div class="listParent">
+        <div class="picture">
+          {" "}
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/054/007/267/small/a-little-girl-smiles-while-wearing-a-yellow-shirt-photo.jpg"
+            width="80px"
+            height="80px"
+            alt="photo"
+          />{" "}
+        </div>
+        <div class="text1">
+          <div> {props.name}</div>
+          <div> @Maribek</div>
+        </div>
+        <div class="text1">
+          <div> {props.date}</div>
+          <div> Subscribe Date</div>
+        </div>
+        <div class="text1">
+          <div> None</div>
+          <div> Renew Date</div>
+        </div>
+        <div class="text1">
+          <div> {props.monthly}</div>
+          <div> Monthly Cc</div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Card;
